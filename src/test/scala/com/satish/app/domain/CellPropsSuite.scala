d17b625ae6e85de47ce51ddaabc1d16d3582b1f8
satish <satish.manit@gmail.com>
2023-01-11 21:47:15 +0530

    add prop test

--- src/test/scala/com/satish/app/domain/CellPropsSuite.scala
+++ src/test/scala/com/satish/app/domain/CellPropsSuite.scala
@@ -1,8 +1,14 @@
 package com.satish.app.domain
 import munit.ScalaCheckSuite
+import org.scalacheck.Gen
 import org.scalacheck.Prop.*
 
 class CellPropsSuite extends ScalaCheckSuite:
+
+  property("I should be able to create cell between 1 and 9"){
+    forAll(Gen.choose(1,9)){ i => Cell(i).isDefined
+    }
+  }
 
   property("I should not be able to create Cell with rank less than 1"){
     forAll{ (i: Int) =>
@@ -15,3 +21,5 @@ class CellPropsSuite extends ScalaCheckSuite:
       (i > 9) ==> (Cell(i).isEmpty)
     }
   }
+
+
